1a5c80aa9a8243d5189ed6ac2887bee2107a0e3c
talle3miu3gi-ctrl <talle3miu3gi@gmail.com>
2026-04-19 19:34:05 +0900

    単体テスト　メールのランダム化

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     nickname              { 'test' }
-    email                 { 'test@example.com' }
+    email                 { Faker::Internet.free_email }
     password              { 'abcd0000' }
     password_confirmation { password }
     last_name             { '山田' }
