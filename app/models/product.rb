ffadecb2a226e0b14864a9d7837e0df1e2e7b10d
talle3miu3gi-ctrl <talle3miu3gi@gmail.com>
2026-04-20 17:02:30 +0900

    active_hashの実装

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,16 @@ class Product < ApplicationRecord
     belongs_to :user
     has_one    :purchased
     has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category  
+  belongs_to :product_status
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :delivery_schedule
+
+validates :category_id, :product_status_id, :shipping_fee_id, :prefecture_id, :delivery_schedule_id, 
+           numericality: { other_than: 1 , message: "can't be blank" }
+
 end
+
