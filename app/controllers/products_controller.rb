b5716204f1c453243948b7c72a645d0f643cbad0
talle3miu3gi-ctrl <talle3miu3gi@gmail.com>
2026-04-23 07:51:50 +0900

    コントローラー・コンフィグの設定

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,9 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_product, only: [:show, :edit, :update]
+  before_action :contributor_confirmation, only: [:edit, :update]
+  before_action :redirect_if_sold_out, only: [:edit, :update]
+
   def index
     @products = Product.all.order('created_at DESC')
   end
@@ -22,11 +26,35 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update 
+    @product = Product.find(params[:id])
+    @product.update(product_params)
+    if @product.save
+      redirect_to product_path(@product)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+
+
 
   private
 
   def product_params
     params.require(:product).permit(:name, :image, :description, :price, :category_id, :product_status_id, :shipping_fee_id,
                                     :prefecture_id, :delivery_schedule_id).merge(user_id: current_user.id)
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def contributor_confirmation
+    redirect_to root_path unless current_user == @product.user
   end
 end
