7eb41c4c67c452115dc9e62b4bad0f20d301da03
talle3miu3gi-ctrl <talle3miu3gi@gmail.com>
2026-04-28 15:48:45 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2026_04_23_065913) do
-  create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
     t.bigint "record_id", null: false
@@ -21,7 +24,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_04_23_065913) do
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-  create_table "active_storage_blobs", charset: "utf8mb3", force: :cascade do |t|
+  create_table "active_storage_blobs", force: :cascade do |t|
     t.string "key", null: false
     t.string "filename", null: false
     t.string "content_type"
@@ -33,13 +36,13 @@ ActiveRecord::Schema[7.1].define(version: 2026_04_23_065913) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "active_storage_variant_records", charset: "utf8mb3", force: :cascade do |t|
+  create_table "active_storage_variant_records", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "addresses", charset: "utf8mb3", force: :cascade do |t|
+  create_table "addresses", force: :cascade do |t|
     t.string "zip_code", null: false
     t.integer "prefecture_id", null: false
     t.string "city", null: false
@@ -52,7 +55,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_04_23_065913) do
     t.index ["purchased_id"], name: "index_addresses_on_purchased_id"
   end
 
-  create_table "products", charset: "utf8mb3", force: :cascade do |t|
+  create_table "products", force: :cascade do |t|
     t.string "name", null: false
     t.text "description", null: false
     t.integer "category_id", null: false
@@ -67,7 +70,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_04_23_065913) do
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
-  create_table "purchaseds", charset: "utf8mb3", force: :cascade do |t|
+  create_table "purchaseds", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "product_id", null: false
     t.datetime "created_at", null: false
@@ -76,7 +79,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_04_23_065913) do
     t.index ["user_id"], name: "index_purchaseds_on_user_id"
   end
 
-  create_table "users", charset: "utf8mb3", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
